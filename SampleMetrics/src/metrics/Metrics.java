a3e080bca81a733bbc47add3c8de47adc00b1a4e
Aleksandr Kolesov <aleksandr_kolesov@epam.com>
2015-10-05 17:07:18 +0300

    no semaphore

--- SampleMetrics/src/metrics/Metrics.java
+++ SampleMetrics/src/metrics/Metrics.java
@@ -1,164 +1,153 @@
 package metrics;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
+import util.BatchBuffer;
+import util.BatchConsumer;
+
+import java.util.Collection;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class Metrics {
 
-	private static final int QUEUE_CAPACITY = 2;
-	public static final int MAX_PAIRS = 1000;
+    private static final int QUEUE_CAPACITY = 2;
+    public static final int MAX_PAIRS = 500;
+    public static final int CORE_POOL_SIZE = 2;
+    public static final int MAXIMUM_POOL_SIZE = 10;
+    public static final int CAPACITY = 1;
 
-	public static void main(final String[] args) {
+    public static void main(final String[] args) {
 
-		System.out.println("Start");
+        System.out.println("Start");
 
-		long start = System.nanoTime();
+        long start = System.nanoTime();
 
-		Metrics metrics = new Metrics();
+        Metrics metrics = new Metrics();
 
-		metrics.doWork();
+        metrics.doWork();
 
-		long stop = System.nanoTime();
-		System.out.println("Finish");
-		System.out.println("Elapsed: " + (stop - start));
-	}
+        long stop = System.nanoTime();
+        System.out.println("Finish");
+        System.out.println("Elapsed: " + (stop - start));
+    }
 
-	private void doWork() {
+    private static final class FullyBlockingQueue<T> extends LinkedBlockingQueue<T> {
 
-		Data data = new Data();
-		ReferenceSequence sequence = new ReferenceSequence();
+        private FullyBlockingQueue(int capacity) {
+            super(capacity);
+        }
 
-		final AtomicLong total = new AtomicLong(0);
+        @Override
+        public boolean offer(T e) {
+            try {
+                put(e);
+                return true;
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
 
-		// Iterator<Record> it = data.iterator();
-		//
-		// while (it.hasNext()) {
-		// Record record = it.next();
-		//
-		// ///
-		// }
+    private void doWork() {
 
-		final ExecutorService service = Executors.newFixedThreadPool(10);
-//		service.
-		final BlockingQueue<List<Object[]>> queue = new LinkedBlockingQueue<List<Object[]>>(
-				QUEUE_CAPACITY);
-		List<Object[]> poisonPill = Collections.emptyList();
-		
-		final Semaphore sem = new Semaphore(6);
+        Data data = new Data();
+        ReferenceSequence sequence = new ReferenceSequence();
 
-		List<Object[]> pairs = new ArrayList<>(MAX_PAIRS);
+        final AtomicLong total = new AtomicLong(0);
 
-		service.execute(new Runnable() {
+        // Iterator<Record> it = data.iterator();
+        //
+        // while (it.hasNext()) {
+        // Record record = it.next();
+        //
+        // ///
+        // }
 
-			@Override
-			public void run() {
-				while (true) {
 
-					try {
-						final List<Object[]> tmpPairs = queue.take();
-						if (tmpPairs.isEmpty()) {
-							return;
-						}
-						
-						sem.acquire();
-						
-						service.submit(new Runnable() {
+        ExecutorService tt = Executors.newCachedThreadPool();
+        final ThreadPoolExecutor service_prcessing = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, 0L, TimeUnit.MILLISECONDS, new FullyBlockingQueue<>(CAPACITY));
+        final ThreadPoolExecutor service_reader = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, 0L, TimeUnit.MILLISECONDS, new FullyBlockingQueue<>(CAPACITY));
+        BatchBuffer<Object[]> batcher = new BatchBuffer<>(MAX_PAIRS, new BatchConsumer<Object[]>() {
+            @Override
+            public void consume(Collection<Object[]> batch) {
+                service_prcessing.execute(new Sum(batch, total));
+            }
+        });
 
-							@Override
-							public void run() {
 
-								int sum = 0;
+        for (final Record record : data) {
 
-//								try {
-//									Thread.sleep(1000);
-//								} catch (InterruptedException e) {
-//									e.printStackTrace();
-//								}
 
-								for (Object[] objects : tmpPairs) {
-									Record rec = (Record) objects[0];
-									Reference ref = (Reference) objects[1];
-									sum += process(rec, ref);
-								}
-
-								total.addAndGet(sum); // CAS compare and swap
-								
-								sem.release();
-							}
-						});
-
-					} catch (InterruptedException e1) {
-						e1.printStackTrace();
-					}
-				}
-			}
-		});
-
-		int pairsCount = 0;
-
-		for (final Record record : data) {
-			final Reference ref = sequence.getRef(record);
+            final Reference ref = sequence.getRef(record);
 //			try {
 //				Thread.sleep(1000);
 //			} catch (InterruptedException e) {
 //				e.printStackTrace();
 //			}
+            batcher.add(new Object[]{record, ref});
 
-			pairs.add(new Object[] { record, ref });
+//
 
-			if (pairs.size() < MAX_PAIRS) {
-				continue;
-			}
+        }
+        batcher.flush();
 
-			pairsCount++;
+    /*    try {
+            queue.put(poisonPill);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
 
-//			System.out.println("lists: " + pairsCount);
+        service_prcessing.shutdown();
 
-			try {
-				queue.put(pairs);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-
-			pairs = new ArrayList<>(MAX_PAIRS);
-
-		}
-		try {
-			queue.put(poisonPill);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-		service.shutdown();
-
-		try {
-			service.awaitTermination(1, TimeUnit.DAYS);
+        try {
+            service_prcessing.awaitTermination(1, TimeUnit.DAYS);
 
 
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+        System.out.println(total.get());
 
-		System.out.println(total.get());
+    }
 
-	}
+    private int process(final Record record, final Reference ref) {
+        int sum = 0;
+        for (int i = 0; i < record.read.length; i++) {
+            sum += record.read[i];
+            sum += ref.read[i];
+        }
+        return sum;
+    }
 
-	private int process(final Record record, final Reference ref) {
-		int sum = 0;
-		for (int i = 0; i < record.read.length; i++) {
-			sum += record.read[i];
-			sum += ref.read[i];
-		}
-		return sum;
-	}
+    private class Sum implements Runnable {
+        private final Collection<Object[]> batch;
+        private final AtomicLong total;
 
+        public Sum(Collection<Object[]> batch, AtomicLong total) {
+            this.batch = batch;
+            this.total = total;
+        }
+
+        @Override
+        public void run() {
+
+            int sum = 0;
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            for (Object[] objects : batch) {
+                Record rec = (Record) objects[0];
+                Reference ref = (Reference) objects[1];
+                sum += process(rec, ref);
+            }
+
+            total.addAndGet(sum); // CAS compare and swap
+
+
+        }
+    }
 }
